d89587d31acf07753d53b6d1ffd380a65b4ee206
mengx520 <mengx520@gmail.com>
2021-11-11 13:37:18 -0800

    Task 2 solution

--- P0/Task2.py
+++ P0/Task2.py
@@ -3,6 +3,8 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+import operator
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -20,3 +22,22 @@ Print a message:
 September 2016.".
 """
 
+number_dict = {}
+
+for call in calls:
+    send_number = call[0]
+    receive_number = call[1]
+
+    if send_number not in number_dict:
+        number_dict[send_number] = 0
+    if receive_number not in number_dict:
+        number_dict[receive_number] = 0
+
+    number_dict[send_number] += int(call[3])
+    number_dict[receive_number] += int(call[3])
+    
+max_time = max(number_dict.items(), key = operator.itemgetter(1))
+
+print(f'{max_time[0]} spent the longest time, {max_time[1]} seconds, on the phone during September 2016.')
+
+# Big(O) = O(n) Linear time
