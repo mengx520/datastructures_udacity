7a837b48cc7d09eedd1e792cf0f6e2f30c09a865
mengx520 <mengx520@gmail.com>
2021-11-11 12:37:53 -0800

    Task 0 solution

--- P0/Task0.py
+++ P0/Task0.py
@@ -20,3 +20,16 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+sending_number = texts[0][0]
+receiving_number = texts[0][1]
+text_time = texts[0][2]
+
+incoming_number = calls[-1][0]
+answering_number = calls[-1][1]
+call_time = calls[-1][2]
+duration = calls[-1][3]
+
+print(f'First record of texts, {sending_number} texts {receiving_number} at time {text_time}')
+print(f'Last record of calls, {incoming_number} calls {answering_number} at time {call_time}, lasting {duration} seconds')
+
+# Big(O) = 1 Constant time
